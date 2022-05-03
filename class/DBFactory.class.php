40cef6489c2dd35bc58d1821bffef91336594869
Felipe Belfort <felipebelfort@gmail.com>
2022-05-03 16:29:22 +0200

    DBFactory and Class autoload implementation

---
+++ class/DBFactory.class.php
@@ -0,0 +1,12 @@
+<?php
+
+class DBFactory
+{
+    public static function getPDOConnexion()
+    {
+        $db = new PDO('mysql:host=localhost; dbname=news', 'root','root');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $db;
+    }
+}
